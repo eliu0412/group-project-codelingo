2cf3fc219d9c64091503902da03dcf1d6d54b12e
YMichaelHao <michael4hao@gmail.com>
2025-02-10 23:47:46 -0500

    Setup Firebase database

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+// Load environment variables
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const admin = require('firebase-admin');
+const session = require('express-session');
+const serviceAccount = require('./keys/serviceAccountKey.json');
+
+const app = express();
+const port = 8080;
+
+// Middleware
+/* TODO: Configure CORS here */
+const corsConfig = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+};
+app.use(cors(corsConfig));
+
+/* TODO: Configure express-session here */
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(express.json());
+
+// Firebase Admin SDK initialization
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+});
+
+const db = admin.database();
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
