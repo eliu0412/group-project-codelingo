223ab5423e79de5c1c0ef3bffb8efbb2d1f6eae8
Kaito Watanabe <kaito.mclean@gmail.com>
2025-02-27 22:11:17 -0500

    basic feature on frontend

--- frontend/src/app/questions/problemPage.tsx
+++ frontend/src/app/questions/problemPage.tsx
@@ -11,7 +11,7 @@ const ProblemPage = () => {
   const [formData, setFormData] = useState<ProblemForm>({
     title: '',
     description: '',
-    variationOptions: []
+    variationOptions: [],
   });
   const [generatedProblem, setGeneratedProblem] = useState<any | null>(null);
   const [loading, setLoading] = useState(false);
@@ -33,7 +33,6 @@ const ProblemPage = () => {
     }));
   };
 
-  // フォーム送信時にgenerateProblemを呼び出す関数
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setLoading(true);
