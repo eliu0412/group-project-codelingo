941f8110a4322f9508d1c681530fada37739c90c
YMichaelHao <michael4hao@gmail.com>
2025-03-07 20:53:49 -0500

    Deleted unessesary stuff

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -2,12 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    apiKey: "AIzaSyAtxjsRtpuIbcEg48AaZfsh-bOuC_2ydTk",
+    authDomain: "codelingo-1.firebaseapp.com",
+    databaseURL: "https://codelingo-1-default-rtdb.firebaseio.com",
+    projectId: "codelingo-1",
+    storageBucket: "codelingo-1.firebasestorage.app",
+    messagingSenderId: "391880921910",
+    appId: "1:391880921910:web:71fd8e36868a8b0dc05997"
 };
 
 const app = initializeApp(firebaseConfig);
