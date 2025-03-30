2e01b1d42a48fc296d08f6cad272ff224d15834b
eliu0412 <80423441+eliu0412@users.noreply.github.com>
2025-03-30 02:16:19 -0400

    fix: server starts not proper

--- backend/lesson-service/server.js
+++ backend/lesson-service/server.js
@@ -1,22 +1,26 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import express from 'express';
-import cors from 'cors';
-import lessonRouter from './src/routes/lessonRoutes.js';
-import bodyParser from 'body-parser';
+import express from "express";
+import cors from "cors";
+import lessonRouter from "./src/routes/lessonRoutes.js";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 8085;
 
 const corsConfig = {
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
-  credentials: true
+  origin: ["http://localhost:3000", "http://localhost:5173"],
+  credentials: true,
 };
 app.use(cors(corsConfig));
 app.use(express.json());
 app.use(bodyParser.json());
 
-app.use('/api/lessons', lessonRouter);
+app.use("/api/lessons", lessonRouter);
 
-export default app;
+const server = app.listen(port, () => {
+  console.log(`Lesson-Service running on http://localhost:${port}`);
+});
+
+export default server;
