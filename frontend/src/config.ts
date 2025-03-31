c5f7a864e2277d1bc34337adfeacbf3005c1afd1
eliu0412 <80423441+eliu0412@users.noreply.github.com>
2025-03-31 17:08:23 -0400

    fix: switch to deployed endpoints

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,4 @@
+/*
  export const config = {
    api: {
      auth: "http://localhost:8081/api/auth",
@@ -7,14 +8,14 @@
      user: "http://localhost:8082/api/user"
    },
  };
- /*
+*/
+
 export const config = {
   api: {
     auth: "http://3.149.235.1:8081/api/auth",
     disc: "http://3.149.235.1:8082/api",
     prob: "http://3.149.235.1:8083/api",
     match: "http://3.149.235.1:8087",
-    user: "http://3.149.235.1:8082/api/user"
+    user: "http://3.149.235.1:8082/api/user",
   },
 };
-*/
