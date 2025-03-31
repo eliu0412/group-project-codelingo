7a61434f408a20f115655d668db9f73726728263
Christian Rogers <135765678+christianGRogers@users.noreply.github.com>
2025-03-31 11:52:01 -0400

    fix: change links

--- frontend/src/app/registration/registrationApi.ts
+++ frontend/src/app/registration/registrationApi.ts
@@ -1,11 +1,11 @@
-export const API_BASE_URL = "http://localhost:8081/api/auth";
+import { config } from '../../config.ts';
 
 export const registerUser = async (email: string, password: string, username: string) => {
-    const response = await fetch("http://localhost:8081/api/auth/register", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password, username }),
+    const response = await fetch(`${config.api.auth}/register`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password, username }),
     });
-
+  
     return response.json();
-};
+  };
--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,7 +1,18 @@
+// export const config = {
+//   api: {
+//     auth: "http://localhost:8081/api/auth",
+//     disc: "http://localhost:8082/api",
+//     prob: "http://localhost:8083/api",
+//     match: "http://localhost:8087",
+//     user: "http://localhost:8082/api/user"
+//   },
+// };
 export const config = {
   api: {
-    disc: "http://localhost:8082/api",
-    prob: "http://localhost:8083/api",
-    match: "http://localhost:8087",
+    auth: "http://3.149.235.1:8081/api/auth",
+    disc: "http://3.149.235.1:8082/api",
+    prob: "http://3.149.235.1:8083/api",
+    match: "http://3.149.235.1:8087",
+    user: "http://3.149.235.1:8082/api/user"
   },
 };
